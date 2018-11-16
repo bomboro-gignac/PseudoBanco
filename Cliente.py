3c8d35e743d69bcf1b833052f6ba2b0b338fd4b5
bomboro-gignac <sam_rm123@ciencias.unam.mx>
2018-11-15 22:53:10 -0600

    se modifico el metodo str pero falta darle formato a la impresion

--- Cliente.py
+++ Cliente.py
@@ -3,11 +3,11 @@
 from Cuenta import *
 
 class Cliente:
-    def __init__(self, nombre, edad, direccion,cuenta):
-        self.__nombre = nombre
-        self.__edad = edad
-        self.__direccion = direccion
-        self.__cuenta = cuenta
+    def __init__(self, nombre, edad, direccion):
+        self.nombre = nombre
+        self.edad = edad
+        self.direccion = direccion
+        self.cuentas = []
     #metodo constructor
     
     """def mostrarDetalles (self):
@@ -15,34 +15,59 @@ class Cliente:
         print ("\n", self.__nombre)
         print ("\n", self.__edad)
         print ("\n", self.__direccion) """
-        
-        # el metodo mostrar detalles lo sustituimos por el metodo __str__
-        
-    def __str__( self ):
+    
+    # el metodo mostrar detalles lo sustituimos por el metodo __str__
+    
+    """def __str__( self ):
         tmp = ""
         tmp += "Nombre::" + str( self.__nombre)
         tmp += "\nDireccion::" + str( self.__direccion)
         tmp += "\nEdad::" + str( self.__edad)
         tmp += "\n" + str(self.__cuenta)
-        return tmp    
+        return tmp"""
     # __str__ sobreescribe el metodo str devolviendo un string
-
+    
     def setedad(self, edad):
-       self.__edad = edad
-
+        self.__edad = edad
+    
     def getedad (self):
-       return self.__edad
-
+        return self.__edad
+    
     def setdireccion(self, direccion):
-       self.__direccion = direccion
-
+        self.__direccion = direccion
+    
     def getdireccion (self):
-       return self.__direccion
+        return self.__direccion
+    
+    #puse los set y get para poder modificar los atributos pues pienso que pueden ir cambiando y no son constantes
+    
+    
+    def agregarCuentas(self,cuenta):
+        self.cuentas.append(cuenta)
+    
+    def eliinarCuentas(self,cuenta):
+        self.cuentas.remove(cuenta)
+    
+    def getCuenta(self,index):
+        return self.cuentas[index]
+    
+    def __str__( self ):
+        tmp = "Nombre::" + str( self.nombre )
+        tmp += "\nDireccion::" + str( self.direccion )
+        tmp += "\nEdad::" + str( self.edad )
+        tmp += "\n" +self.cteCuentas()
+        return tmp
+    
+    def cteCuentas( self ):
+        straux = ""
+            for i in range( len(self.cuentas)):
+                if type(self.cuentas[i]) is "'CuentaCredito.CuentaCredito'" :
+                    print ( str(type(self.cuentas[i])) + " : ")
+                        else:
+                            print ( str(type(self.cuentas[i])) + " ++ ")
+                                #straux= straux+str(i)+ "  " + str(self.cuentas[i] )
+                                return straux
 
-    #puse los set y get para poder modificar los atributos pues pienso que pueden ir cambiando y no son constantes   
-       
 
-      
-           
 
 
--- Pruebas.py
+++ Pruebas.py
@@ -1,5 +1,3 @@
-#elaboro samuel 22/010/18
-
 from Cuenta import *
 from Cliente import *
 from CuentaHija import *
@@ -10,75 +8,82 @@ class Pruebas:
     pass
 print ("Desde las pruebas")
 print()
-Cuenta1 = Cuenta(300)
-print()
-print (Cuenta1.consulta())
-Cuenta1.depositar(330)
-Cuenta1.retirar(1000)
-print (Cuenta1.consulta())
+"""Cuenta1 = Cuenta(300)
+    print()
+    print (Cuenta1.consulta())
+    Cuenta1.depositar(330)
+    Cuenta1.retirar(1000)
+    print (Cuenta1.consulta())
+    
+    print()
+    
+    Cuenta2 = Cuenta(4000)
+    print (Cuenta2)
+    
+    Cuenta2.setCantidad(200)
+    print(Cuenta2)
+    print()
+    
+    Cuenta3 = CuentaHija(2000, "credito" )
+    print(Cuenta3)
+    Cuenta3.depositar(330)
+    Cuenta3.retirar(10000)
+    
+    print()
+    
+    Cliente1 = Cliente ("Juan", 34, "coyoacan", Cuenta1)
+    
+    Cliente1.setdireccion("Xochimilco")
+    Cliente2 = Cliente ("Tommy",23,"tlalpan", Cuenta2)
+    print(Cliente1)
+    Cliente2.setedad(45)
+    print()
+    
+    print(Cliente2)
+    
+    print()
+    Cuenta8 = CuentaCredito(200,1000)
+    Cuenta8.retirar(1300)
+    
+    print()
+    
+    print(Cuenta8)
+    a = Cuenta8.getSobregiro()
+    print(a)
+    
+    Cliente9 = Cliente ("pepe", 36, "coyoacan", Cuenta8)
+    
+    print()
+    
+    print(Cliente9)
+    
+    
+    Cuenta9 = CuentaDeAhorro(200,.02)
+    Cliente10 = Cliente ("Aldo", 39, "Sta ursula", Cuenta9)
+    print()
+    
+    print(Cliente10)
+    
+    print()
+    Cuenta10 = CuentaCredito(300,3000)
+    Cuenta10.retirar(200)
+    print(Cuenta10)
+    
+    print()
+    Cuenta11 = CuentaCredito(400,3000)
+    Cuenta11.retirar(400)
+    print(Cuenta11)
+    
+    print()
+    
+    Cuenta12 = CuentaCredito(500,2000)
+    Cuenta12.retirar(600)
+    print(Cuenta12)"""
 
-print()
+Cuenta13 = CuentaCredito(400,800)
 
-Cuenta2 = Cuenta(4000)
-print (Cuenta2)
-
-Cuenta2.setCantidad(200)
-print(Cuenta2)
-print()
-
-Cuenta3 = CuentaHija(2000, "credito" )
-print(Cuenta3)
-Cuenta3.depositar(330)
-Cuenta3.retirar(10000)
-
-print()
-
-Cliente1 = Cliente ("Juan", 34, "coyoacan", Cuenta1)
-
-Cliente1.setdireccion("Xochimilco")
-Cliente2 = Cliente ("Tommy",23,"tlalpan", Cuenta2)
+Cuenta14 = CuentaDeAhorro(400,.1)
+Cliente1 = Cliente ("Juan", 34, "coyoacan")
+Cliente1.agregarCuentas(Cuenta13)
+Cliente1.agregarCuentas(Cuenta14)
 print(Cliente1)
-Cliente2.setedad(45)
-print()
-
-print(Cliente2)
-
-print()
-Cuenta8 = CuentaCredito(200,1000)
-Cuenta8.retirar(1300)
-
-print()
-
-print(Cuenta8)
-a = Cuenta8.getSobregiro()
-print(a)
-
-Cliente9 = Cliente ("pepe", 36, "coyoacan", Cuenta8)
-
-print()
-
-print(Cliente9)
-
-
-Cuenta9 = CuentaDeAhorro(200,.02)
-Cliente10 = Cliente ("Aldo", 39, "Sta ursula", Cuenta9)
-print()
-
-print(Cliente10)
-
-print()
-Cuenta10 = CuentaCredito(300,3000)
-Cuenta10.retirar(200)
-print(Cuenta10)
-
-print()
-Cuenta11 = CuentaCredito(400,3000)
-Cuenta11.retirar(400)
-print(Cuenta11)
-
-print()
-
-Cuenta12 = CuentaCredito(500,2000)
-Cuenta12.retirar(600)
-print(Cuenta12)
-
